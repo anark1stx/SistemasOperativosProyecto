1b079b52964156897670c8c35a37d5fea5df0f80
mateoc <mateosabe66@gmail.com>
2020-10-01 18:29:05 -0300

    modified:   Gestion_SI/.my.cnf 	modified:   Gestion_SI/Mantenimiento/Automatizacion/Redes/configs/ARCHIVOS/firewall 	modified:   Gestion_SI/Mantenimiento/Automatizacion/Redes/configs/ARCHIVOS/interface 	modified:   Gestion_SI/Mantenimiento/Automatizacion/Redes/configs/ARCHIVOS/ssh 	modified:   Gestion_SI/Mantenimiento/Automatizacion/Redes/configs/DHCP/interface 	modified:   Gestion_SI/Mantenimiento/Automatizacion/Redes/configs/DNS/forward.overclode.sibim 	modified:   Gestion_SI/Mantenimiento/Automatizacion/Redes/configs/DNS/interface 	modified:   Gestion_SI/Mantenimiento/Automatizacion/Redes/configs/DNS/named.conf 	modified:   Gestion_SI/Mantenimiento/Automatizacion/Redes/configs/DNS/reverse.overclode.sibim 	modified:   Gestion_SI/Mantenimiento/Automatizacion/Redes/configs/RESPALDOS/interface 	modified:   Gestion_SI/Mantenimiento/Automatizacion/Redes/configs/RESPALDOS/ssh 	deleted:    Gestion_SI/Mantenimiento/Automatizacion/Redes/scripts/ARCHIVOS/a.out 	modified:   Gestion_SI/Mantenimiento/Automatizacion/UsuariosYGrupos/ulist.txt 	modified:   Gestion_SI/Mantenimiento/Automatizacion/scripts_cron/backup_home.sh 	modified:   Gestion_SI/Mantenimiento/Respaldar/Linux/funciones.sh 	modified:   Gestion_SI/Mantenimiento/Respaldar/MySQL/funciones.sh 	modified:   Gestion_SI/mis_rutinas 	modified:   LICENSE 	modified:   README.md 	modified:   config_terminal.ps1

--- Gestion_SI/Mantenimiento/Automatizacion/scripts_cron/backup_home.sh
+++ Gestion_SI/Mantenimiento/Automatizacion/scripts_cron/backup_home.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-tar -zcvpf /tmp/backups/homeT-$(date +%d-%m-%Y).tar.gz /home
+file="homeT-$(date +%d-%m-%Y).tar.gz"
+tar -zcpf "$file" /home && rsync --remove-source-files -raz -e 'ssh -p 49555' $file admin@192.168.0.250:/backup/home
