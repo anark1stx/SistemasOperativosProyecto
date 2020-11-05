ce5e8f03071cc01fff222788dd2befa32152ec00
Mateo Carriquí <mateosabe66@gmail.com>
2020-11-05 17:26:17 -0300

    Update archivos-config.sh

--- Gestion_SI/Mantenimiento/Automatizacion/Redes/scripts/ARCHIVOS/archivos-config.sh
+++ Gestion_SI/Mantenimiento/Automatizacion/Redes/scripts/ARCHIVOS/archivos-config.sh
@@ -3,7 +3,7 @@
 #ESTE SCRIPT NO DEBE SER EJECUTADO DIRECTAMENTE, ABRIR DESDE EL MENU PRINCIPAL.
 #NOTA: en caso de estar usando un servicio virtualizado, el gateway del adaptador de red de la PC en la que esté corriendo esta máquina debe cambiarse a la especificada en la carpeta (192.168.0.1).
 
-./Mantenimiento/Automatizacion/UsuariosYGrupos/main.sh && echo "usuarios y grupos creados con exito."
+./Mantenimiento/Automatizacion/UsuariosYGrupos/main.sh && echo "usuarios y grupos creados con exito." || echo "$(date '+%d/%m/%Y %H:%M:%S'): error creando grupos y usuarios, abortando" ; exit 
 
 mi_interfaz="Mantenimiento/Automatizacion/Redes/configs/ARCHIVOS/interface" #archivo preconfigurado de la interfaz
 mi_ssh="Mantenimiento/Automatizacion/Redes/configs/ARCHIVOS/ssh"
@@ -36,16 +36,15 @@ ifdown $interfaz
 ifup $interfaz
 systemctl restart network
 
-test_conn=$(ping -q -t 5 -w1 -c1 192.168.0.253 && grep "100% packet loss")
+test_conn=$(ping -q -t 5 -w1 -c1 192.168.0.5 && grep "100% packet loss")
 if [[ -z "$test_conn" ]]; then
 	echo "Red configurada con exito."
 else
-	echo "Hubieron errores configurando la red. Verifique que el servidor DNS haya sido configurado y esté encendido."
+	echo "Hubieron errores comunicandose con el servidor de respaldos. Verifique que el servidor de respaldos esté encendido y que haya sido configurado."
 	exit
 fi
 
-
-echo "Instalando ssh, rsync, crontab y firewalld."; yum install -q -y mysql-server openssh-server openssh-clients sshpass rsync crontab firewalld httpd &>/dev/null
+echo "Instalando mysql, git, ssh, rsync, crontab y firewalld."; yum install -q -y mysql-server git openssh-server openssh-clients sshpass rsync crontab firewalld httpd &>/dev/null
 
 systemctl start sshd
 systemctl start firewalld
@@ -93,30 +92,26 @@ sed -i "/SELINUX=enforcing/c SELINUX=disabled" /etc/sysconfig/selinux  #deshabil
 su admin -c cat /dev/zero | ssh-keygen -q -N ""
 
 adminpwd=$(grep -w "admin" Mantenimiento/Automatizacion/UsuariosYGrupos/ulist.txt | cut -d ":" -f2)
-copiar_id=$(su admin -c sshpass -p "$adminpwd" ssh-copy-id "-p 49555" admin@192.168.0.250 | grep "denied\|ERROR")
+copiar_id=$(su admin -c sshpass -p "$adminpwd" ssh-copy-id "-p 49555" admin@192.168.0.5 | grep "denied\|ERROR")
 
 if [[ -n "$copiar_id"  ]]; then
 	echo "Error copiando la clave SSH, verifique que el servidor de respaldos este encendido y conectado"
 	exit
 fi
 
-cat $mi_ssh > /etc/ssh/sshd_config  #aplico recien ahora la configuracion ya que sino no podia conectarme con usuario y contraseña, con esta configuracion habilitada solo se puede entrar con SSHKEY.
+cat $mi_ssh > /etc/ssh/sshd_config
+
+sudo firewall-cmd --add-port=49555/tcp --permanent
+sudo firewall-cmd --remove-port=22/tcp
 systemctl restart sshd
 
 #este archivo guarda las credenciales y cada vez que efectuo mysqldump no tengo que especificar contrasena.
-
 cp .my.cnf /home/admin/.my.cnf
 chown admin: /home/admin/.my.cnf
 chmod 600 ~/.my.cnf
 
 #copio todos los scripts al directorio acordado
 cp -R Mantenimiento/Automatizacion/scripts_cron /var/scripts_cron
-
-#guardo la contrasena del usuario admin, cuando crontab ejecute comandos que requieran sudo paso la contrasena del usuario admin por stdin usando la contrasena que esta guardada en ese archivo
-touch /var/creds.txt
-chown admin: /var/creds.txt
-chmod 600 /var/creds.txt
-echo $adminpwd > /var/creds.txt
 
 #copio el archivo de rutinas de cron
 cp mis_rutinas /var/mis_rutinas
