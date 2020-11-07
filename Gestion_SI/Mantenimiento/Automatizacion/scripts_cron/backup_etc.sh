c6cba412da8f62684fe1cb8d5616edb4bf3e84cc
anark1stx <mateosabe66@gmail.com>
2020-11-07 20:20:17 -0300

    script backup home y etc

--- Gestion_SI/Mantenimiento/Automatizacion/scripts_cron/backup_etc.sh
+++ Gestion_SI/Mantenimiento/Automatizacion/scripts_cron/backup_etc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-su admin -c "rsync --port=49555 -avr /home admin@192.168.0.5:/backup/Linux/home"
+su admin -c "rsync --port=49555 -avr /etc admin@192.168.0.5:/backup/Linux/home"
 
 #ejecuto rsync como usuario admin puesto que es el usuario que por defecto tiene copiada
 su clave ssh en el servidor de respaldos cuando ejecuto el script de automatizacion.
