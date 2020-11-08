01cb323de47e39ef17d86addcc1d595295867f09
anark1stx <mateosabe66@gmail.com>
2020-11-08 13:13:25 -0300

    modified:   Gestion_SI/Mantenimiento/Automatizacion/UsuariosYGrupos/env.sh

--- Gestion_SI/Mantenimiento/Automatizacion/UsuariosYGrupos/env.sh
+++ Gestion_SI/Mantenimiento/Automatizacion/UsuariosYGrupos/env.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-set -x 
-set -n
-adminpwd=$(grep -w "admin" Mantenimiento/Automatizacion/UsuariosYGrupos/ulist.txt | cut -d ":" -f2)
-echo "export $adminpwd" >> /etc/profile.d/adminpwd.sh #export para poder seguir usandola fuera del script
+echo "export adminpwd=overcl0de-4DM" >> /etc/profile #export para poder seguir usandola fuera del script
+source /etc/profile
