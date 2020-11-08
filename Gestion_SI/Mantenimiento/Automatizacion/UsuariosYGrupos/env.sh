397816e35d1803395abfce74bf9490319486719b
Mateo Carriquí <mateosabe66@gmail.com>
2020-11-08 12:59:09 -0300

    Update env.sh

--- Gestion_SI/Mantenimiento/Automatizacion/UsuariosYGrupos/env.sh
+++ Gestion_SI/Mantenimiento/Automatizacion/UsuariosYGrupos/env.sh
@@ -1,2 +1,5 @@
-#!/bin/bash -x
-export adminpwd=$(grep -w "admin" Mantenimiento/Automatizacion/UsuariosYGrupos/ulist.txt | cut -d ":" -f2) #export para poder seguir usandola fuera del script
+#!/bin/bash
+set -x 
+set -n
+adminpwd=$(grep -w "admin" Mantenimiento/Automatizacion/UsuariosYGrupos/ulist.txt | cut -d ":" -f2)
+echo "export $adminpwd" >> /etc/profile.d/adminpwd.sh #export para poder seguir usandola fuera del script
