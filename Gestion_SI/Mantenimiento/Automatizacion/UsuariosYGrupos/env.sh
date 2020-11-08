fda0c01e462a4c90f385e48e04a11d05b3385b67
Mateo Carriquí <mateosabe66@gmail.com>
2020-11-08 12:35:10 -0300

    Update env.sh

--- Gestion_SI/Mantenimiento/Automatizacion/UsuariosYGrupos/env.sh
+++ Gestion_SI/Mantenimiento/Automatizacion/UsuariosYGrupos/env.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
 export adminpwd=$(grep -w "admin" Mantenimiento/Automatizacion/UsuariosYGrupos/ulist.txt | cut -d ":" -f2) #export para poder seguir usandola fuera del script
-echo "pwd : $adminpwd"
