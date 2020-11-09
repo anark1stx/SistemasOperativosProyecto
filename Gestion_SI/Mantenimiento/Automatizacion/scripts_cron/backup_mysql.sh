8e45c5837b17c21cfdcccf8dc88cc84914212a74
Mateo Carriquí <mateosabe66@gmail.com>
2020-11-09 12:28:49 -0300

    Update backup_mysql.sh

--- Gestion_SI/Mantenimiento/Automatizacion/scripts_cron/backup_mysql.sh
+++ Gestion_SI/Mantenimiento/Automatizacion/scripts_cron/backup_mysql.sh
@@ -7,7 +7,7 @@ rsync -e "ssh -i /home/admin/.ssh/id_rsa -p 49555" -az mysql.sql admin@192.168.0
 rsync -e "ssh -i /home/admin/.ssh/id_rsa -p 49555" -az sibim.sql admin@192.168.0.5:/backup/SIBIM-BDS/ && echo "BD SIBIM respaldado con exito" >> /logs/respaldos.log || echo "Hubo errores respaldando BD SIBIM" >> /logs/respaldos.log
 
 ssh admin@192.168.0.5 -i /home/admin/.ssh/id_rsa -p 49555 'cd /backup/SIBIM-BDS && git config --global user.email "overclodep@gmail.com" ; git config --global user.name "admin"'
-ssh admin@192.168.0.5 -i /home/admin/.ssh/id_rsa -p 49555 'cd /backup/SIBIM-BDS && git add -A && git commit -m "Respaldo BDS $1"'
+ssh admin@192.168.0.5 -i /home/admin/.ssh/id_rsa -p 49555 'cd /backup/SIBIM-BDS && sudo git add -A && sudo git commit -m "Respaldo BDS $1"'
 #ejecuto rsync como usuario admin puesto que es el usuario que por defecto tiene copiada su clave ssh en el servidor de respaldos cuando ejecuto el script de automatizacion.
 rm -rf mysql.sql
 rm -rf sibim.sql
