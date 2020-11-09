d1c67e947e2fbf5ae5928f7f74c91f4050add984
Mateo Carriquí <mateosabe66@gmail.com>
2020-11-09 00:35:14 -0300

    Add files via upload

--- Gestion_SI/sibimcompleto22.sql
+++ Gestion_SI/sibimcompleto22.sql
@@ -14,6 +14,13 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+--
+-- Current Database: `sibim`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `sibim` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `sibim`;
 
 --
 -- Table structure for table `analisis`
