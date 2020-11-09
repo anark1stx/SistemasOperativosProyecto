90e07dfc2b3d793889f025eb4e9cb443e3c13b2c
Mateo Carriquí <mateosabe66@gmail.com>
2020-11-08 22:53:52 -0300

    Update restaurar_linux.sh

--- Gestion_SI/Mantenimiento/Restaurar/restaurar_linux.sh
+++ Gestion_SI/Mantenimiento/Restaurar/restaurar_linux.sh
@@ -4,7 +4,7 @@ test -f $sshkey && echo "Clave SSH encontrada, procediendo a conectarse con el r
 
 echo "A continuacion sera mostrado todo el historial de respaldos, luego de que haya localizado el que quiere restaurar, copie los primeros 6 digitos del SHA, y escribalos, el sistema se restaurara automaticamente en ese punto."ssh -i $sshkey /homeadmin@192.168.0.5 -p 49555 'cd /backup/Linux && git log'
 read -p "Ingrese los primeros 6 digitos del SHA del commit que desea respaldar: " sha
-echo "Copiando el repositorio." ; rsync -e "ssh -i /home/admin/.ssh/id_rsa -p 49555" -chavzp admin@192.168.0.5:/backup/Linux /var && echo "Repositorio copiado con exito" || (echo "hubo errores copiando el repositorio." ; exit)
+echo "Copiando el repositorio." ; rsync -e "ssh -i $sshkey -p 49555" -chavzp admin@192.168.0.5:/backup/Linux /var && echo "Repositorio copiado con exito" || (echo "hubo errores copiando el repositorio." ; exit)
 echo "SHA seleccionado fue: $sha, restaurandolo." ; cd /var/Linux && git reset --hard $sha && echo "Repositorio restaurado a la version indicada" || (echo "Hubo errores restaurando el repositorio" ; exit)
 cd /var #me muevo a var para no tener problemas al sobreescribir home
 cp -R home /home && echo "Directorio home respaldado con exito" || "Hubo errores al sobreescribir el directorio home"
