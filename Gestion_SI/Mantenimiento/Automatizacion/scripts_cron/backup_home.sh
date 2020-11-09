f1268d5cbbf786c5a02639e1e4894e67fedc54b6
Mateo Carriquí <mateosabe66@gmail.com>
2020-11-09 11:45:21 -0300

    Update backup_home.sh

--- Gestion_SI/Mantenimiento/Automatizacion/scripts_cron/backup_home.sh
+++ Gestion_SI/Mantenimiento/Automatizacion/scripts_cron/backup_home.sh
@@ -4,5 +4,5 @@ echo "$(date '+%d/%m/%Y %H:%M:%S'): Respaldando directorio HOME $1 " >> /logs/re
 rsync -e "ssh -i /home/admin/.ssh/id_rsa -p 49555" -arz /home admin@192.168.0.5:/backup/Linux/ && echo "Directorio HOME respaldado con exito" >> /logs/respaldos.log || echo "Hubo errores respaldando directorio home" >> /logs/respaldos.log
 
 ssh admin@192.168.0.5 -i /home/admin/.ssh/id_rsa -p 49555 'cd /backup/Linux && git config --global user.email "overclodep@gmail.com" ; git config --global user.name "admin"'
-ssh admin@192.168.0.5 -i /home/admin/.ssh/id_rsa -p 49555 'cd /backup/Linux && git add -A && git commit -m "Respaldo HOME $1"'
+ssh admin@192.168.0.5 -i /home/admin/.ssh/id_rsa -p 49555 'cd /backup/Linux && git add -A --ignore-errors && git commit -m "Respaldo HOME $1"'
 #ejecuto rsync como usuario admin puesto que es el usuario que por defecto tiene copiada su clave ssh en el servidor de respaldos cuando ejecuto el script de automatizacion.
