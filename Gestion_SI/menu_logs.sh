b06b9e76c095d98db497d1388748e4e1a0b9dcbc
Mateo Carriquí <mateosabe66@gmail.com>
2020-11-09 03:46:26 -0300

    Create menu_logs.sh

---
+++ Gestion_SI/menu_logs.sh
@@ -0,0 +1,57 @@
+#!/bin/bash 
+opcion0="99"
+clear
+while [ "$opcion0" != "0"  ] 
+do
+	echo "---------------------------------------------------------"
+	echo "|		Menu de visualizacion de logs           |"
+	echo "|							                              |"
+	echo "|1- Ver logs de instalaciones  			        |"
+	echo "|2- Ver logs de respaldos					          |"
+	echo "|3- Ver logs de restauraciones        			|"
+	echo "|4- Ver las ultimas 100 entradas de rsyslog |"
+	echo "|5- Ver logs de rsyslog por fecha           |"
+  echo "|6- Ver logs de rsyslog por servicio        |"
+	echo "|0- Salir					                        	|"
+	echo "---------------------------------------------------------"
+	read -p "> " opcion0
+
+	case "$opcion0" in 
+		"1")
+			clear
+			cat /logs/resultados_scripts.log
+			;;
+		"2")
+			clear
+			cat /logs/respaldos.log
+			;;
+		"3")
+			clear
+			cat /logs/restauraciones.log
+			;;
+		"4")
+			clear
+			tail -100 /var/log/secure 
+			;;
+		"5")
+			clear
+      read -n "Ingrese la fecha y/u hora (Formato: Nov 9 03:32): " fecha
+			cat /var/log/secure | grep $fecha
+			;;
+    "6")
+			clear
+      read -n "Ingrese el nombre del servicio a buscar: " sv
+			cat /var/log/secure | grep $sv
+			;;
+		"0")
+			clear
+			opcion0="0"
+			;;
+
+		*)	
+			clear
+			echo "Opcion invalida"
+			;;
+	esac
+
+done
