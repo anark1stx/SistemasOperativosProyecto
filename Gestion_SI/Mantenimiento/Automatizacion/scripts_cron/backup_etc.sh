748da5f4eddbaa74dcd8bf12119246314625d108
Mateo Carriquí <mateosabe66@gmail.com>
2020-11-09 11:34:10 -0300

    Update backup_etc.sh

--- Gestion_SI/Mantenimiento/Automatizacion/scripts_cron/backup_etc.sh
+++ Gestion_SI/Mantenimiento/Automatizacion/scripts_cron/backup_etc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # $1 = tipo de respaldo
 echo "$(date '+%d/%m/%Y %H:%M:%S'): Respaldando directorio ETC $1 " >> /logs/respaldos.log
-rsync -e "ssh -i /home/admin/.ssh/id_rsa -p 49555" -arz --exclude 'gshadow' /etc admin@192.168.0.5:/backup/Linux/ && echo "Directorio ETC respaldado con exito" >> /logs/respaldos.log || echo "Hubo errores respaldando directorio etc" >> /logs/respaldos.log
+rsync -e "ssh -i /home/admin/.ssh/id_rsa -p 49555" -arz --exclude 'gshadow' /etc admin@192.168.0.5:/backup/Linux/ &>/dev/null && echo "Directorio ETC respaldado con exito" >> /logs/respaldos.log || echo "Hubo errores respaldando directorio etc" >> /logs/respaldos.log
 
 ssh admin@192.168.0.5 -i /home/admin/.ssh/id_rsa -p 49555 'cd /backup/Linux && git config --global user.email "overclodep@gmail.com" ; git config --global user.name "admin"'
 ssh admin@192.168.0.5 -i /home/admin/.ssh/id_rsa -p 49555 'cd /backup/Linux && git add -A && git commit -m "Respaldo ETC $1"'
