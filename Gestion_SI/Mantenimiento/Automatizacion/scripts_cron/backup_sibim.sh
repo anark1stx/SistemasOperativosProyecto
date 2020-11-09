6e29293b027025bb51a80cbc16be015baa80fc84
Mateo Carriquí <mateosabe66@gmail.com>
2020-11-09 03:13:14 -0300

    Update backup_sibim.sh

--- Gestion_SI/Mantenimiento/Automatizacion/scripts_cron/backup_sibim.sh
+++ Gestion_SI/Mantenimiento/Automatizacion/scripts_cron/backup_sibim.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 # $1 = tipo de respaldo
 echo "$(date '+%d/%m/%Y %H:%M:%S'): Respaldando BD SIBIM $1 " >> /logs/respaldos.log
-mysqldump -u root mysql -p$adminpwd > dump.sql && echo "dump generado" || echo "hubieron errores efecutando dump" 
-rsync -e "ssh -i /home/admin/.ssh/id_rsa -p 49555" -arz /home admin@192.168.0.5:/backup/Linux/ && echo "Directorio HOME respaldado con exito" >> /logs/respaldos.log || echo "Hubo errores respaldando directorio h
-ome" >> /logs/respaldos.log
+mysqldump -u root sibim -p$adminpwd > sibim.sql && echo "dump generado" || echo "hubieron errores efecutando dump" 
+rsync -e "ssh -i /home/admin/.ssh/id_rsa -p 49555" -az sibim.sql admin@192.168.0.5:/backup/SIBIM-BDS/ && echo "BD SIBIM respaldado con exito" >> /logs/respaldos.log || echo "Hubo errores respaldando BD SIBIM" >> /logs/respaldos.log
 
 ssh admin@192.168.0.5 -i /home/admin/.ssh/id_rsa -p 49555 'cd /backup/SIBIM-BDS && git config --global user.email "overclodep@gmail.com" ; git config --global user.name "admin"'
-ssh admin@192.168.0.5 -i /home/admin/.ssh/id_rsa -p 49555 'cd /backup/SIBIM-BDS && git add -A && git commit -m "Respaldo HOME $1"'
+ssh admin@192.168.0.5 -i /home/admin/.ssh/id_rsa -p 49555 'cd /backup/SIBIM-BDS && git add -A && git commit -m "Respaldo SIBIM $1"'
 #ejecuto rsync como usuario admin puesto que es el usuario que por defecto tiene copiada su clave ssh en el servidor de respaldos cuando ejecuto el script de automatizacion.
-rm -rf dump.sql
+rm -rf sibim.sql
