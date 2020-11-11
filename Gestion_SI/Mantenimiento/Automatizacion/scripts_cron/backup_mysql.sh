0e4502747fe3f5b3bced570851edcba909580acb
Mateo Carriquí <mateosabe66@gmail.com>
2020-11-11 09:39:13 -0300

    Update backup_mysql.sh

--- Gestion_SI/Mantenimiento/Automatizacion/scripts_cron/backup_mysql.sh
+++ Gestion_SI/Mantenimiento/Automatizacion/scripts_cron/backup_mysql.sh
@@ -3,8 +3,8 @@
 echo "$(date '+%d/%m/%Y %H:%M:%S'): Respaldando BD MYSQL $1 " >> /logs/respaldos.log
 mysqldump -u root mysql -p$adminpwd --databases > mysql.sql && echo "dump generado" || (echo $(date '+%d/%m/%Y %H:%M:%S'): "hubieron errores efecutando dump mysql" ; exit)
 mysqldump -u root sibim -p$adminpwd --routines --databases > sibim.sql && echo "dump generado" || (echo $(date '+%d/%m/%Y %H:%M:%S'): "hubieron errores efecutando dump sibim" ; exit)
-rsync -e "ssh -p 49555" -az mysql.sql admin@192.168.0.5:/backup/SIBIM-BDS/ && echo "BD MYSQL respaldado con exito" >> /logs/respaldos.log || echo "Hubo errores respaldando BD MYSQL" >> /logs/respaldos.log
-rsync -e "ssh -p 49555" -az sibim.sql admin@192.168.0.5:/backup/SIBIM-BDS/ && echo "BD SIBIM respaldado con exito" >> /logs/respaldos.log || echo "Hubo errores respaldando BD SIBIM" >> /logs/respaldos.log
+rsync -e "ssh -p 49555" -az mysql.sql root@192.168.0.5:/backup/SIBIM-BDS/ && echo "BD MYSQL respaldado con exito" >> /logs/respaldos.log || echo "Hubo errores respaldando BD MYSQL" >> /logs/respaldos.log
+rsync -e "ssh -p 49555" -az sibim.sql root@192.168.0.5:/backup/SIBIM-BDS/ && echo "BD SIBIM respaldado con exito" >> /logs/respaldos.log || echo "Hubo errores respaldando BD SIBIM" >> /logs/respaldos.log
 
 ssh root@192.168.0.5 -p 49555 'cd /backup/SIBIM-BDS && git config --global user.email "overclodep@gmail.com" ; git config --global user.name "admin"'
 ssh root@192.168.0.5 -p 49555 'cd /backup/SIBIM-BDS && sudo git add -A && sudo git commit -m "Respaldo BDS $1"'
