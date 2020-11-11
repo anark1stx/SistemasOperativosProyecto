af75cc50832b22ce666b192fb6b54fca3e346da9
Mateo Carriquí <mateosabe66@gmail.com>
2020-11-11 00:40:32 -0300

    Update restaurar_mysql.sh

--- Gestion_SI/Mantenimiento/Restaurar/restaurar_mysql.sh
+++ Gestion_SI/Mantenimiento/Restaurar/restaurar_mysql.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 test -f /home/admin/.ssh/id_rsa && echo "Clave SSH encontrada, procediendo a conectarse con el repositorio remoto" || ( echo "No se encontraron claves SSH para el usuario admin, primero debe correr el script de instalacion para este servidor (ARCHIVOS)"  >> /logs/restauraciones.log ; exit )
 cd /
-echo "$(date '+%d/%m/%Y %H:%M:%S'): Restaurando directorio HOME" >> /logs/respaldos.log
+echo "$(date '+%d/%m/%Y %H:%M:%S'): Restaurando bases de datos" >> /logs/respaldos.log
 echo "A continuacion sera mostrado todo el historial de respaldos, luego de que haya localizado el que quiere restaurar, copie los primeros 6 digitos del SHA, y escribalos, el sistema se restaurara automaticamente en ese punto." ; ssh -i /home/admin/.ssh/id_rsa -p 49555 admin@192.168.0.5 'cd /backup/SIBIM-BDS && git log'
 read -p "Ingrese los primeros 6 digitos del SHA del commit que desea respaldar: " sha
 echo "Copiando el repositorio." ; rsync -e "ssh -i /home/admin/.ssh/id_rsa -p 49555" -chavzp admin@192.168.0.5:/backup/SIBIM-BDS /var && echo "Repositorio copiado con exito" || (echo "hubo errores copiando el repositorio." >> /logs/respaldos.log ; exit)
